e3f23f3287cd641b1bab288771f2242c0357c5f0
matthewheck17 <57510061+matthewheck17@users.noreply.github.com>
2021-03-31 13:54:47 -0400

    removing all references to users

--- to_do_listDB.sql
+++ to_do_listDB.sql
@@ -1,18 +1,9 @@
-CREATE TABLE users (
-    user_id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    username VARCHAR(25),
-    password VARCHAR(100)
-);
-
 CREATE TABLE tasks (
 	task_id int NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     content VARCHAR(1500) NOT NULL,
     completed BOOL DEFAULT false
 );
-
-INSERT INTO users (username, password)
-VALUES ('test','test');
 
 INSERT INTO tasks (title, content)
 VALUES ('Test task','This is a test task');
