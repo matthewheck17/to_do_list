651b9db24c98fe2d5c405f4711e1a15f5ac9f9ad
mauleenn <mauleen.ndlovu@outlook.com>
2021-04-01 14:18:24 -0500

    Added new screen for users to add a new task
    A new task includes the name and the description. The add button now redirects to the "Add a New Task" page.

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,21 +1,35 @@
 package com.example.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Add button
+        val arrayAdapter: ArrayAdapter<*>
+        val tasks = arrayOf("Pass Advanced Internet Concepts", "Buy face care stuff", "Buy Jackie a heat lamp", "Bake a cake", "Move in to new house")
+
+        // access the listView from xml file
+        var tasksList = findViewById<ListView>(R.id.tasksList)
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, tasks)
+        tasksList.adapter = arrayAdapter
+
+        // When the user presses the add button they are redirected
+        // to the "Add a New Task" page. On this page the user can
+        // add a new task and add the description.
         val add = findViewById<FloatingActionButton>(R.id.add)
-        add.setOnClickListener {
-            // We are showing only toast message. However, you can do anything you need.
-            Toast.makeText(applicationContext, "This is the add button", Toast.LENGTH_SHORT).show()
+        add.setOnClickListener{
+            val intent = Intent(this, taskDescription::class.java)
+            startActivity(intent)
         }
 
         // Delete button
@@ -24,65 +38,5 @@ class MainActivity : AppCompatActivity() {
             // We are showing only toast message. However, you can do anything you need.
             Toast.makeText(applicationContext, "This is the delete button", Toast.LENGTH_SHORT).show()
         }
-
-        // Initializing the array lists and the adapter
-        var itemlist = arrayListOf<String>()
-        var adapter =ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, itemlist)
-
-
-        // Adding the items to the list when the add button is pressed
-        add.setOnClickListener {
-
-        }
-
-        // Selecting and deleting the items from the list when the delete button is pressed
-        delete.setOnClickListener {
-
-        }
-
-        /*
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
-            // Initializing the array lists and the adapter
-            var itemlist = arrayListOf<String>()
-            var adapter = ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_multiple_choice, itemlist
-            )
-
-            FloatingActionButton add = (FloatingActionButton)
-            findViewById(R.id.add);
-            add.bringToFront();
-
-            // Adding the items to the list when the add button is pressed
-            add.setOnClickListener(new View.OnClickListener() {
-
-
-                itemlist.add(addNewItem.text.toString())
-                listView.adapter = adapter
-                adapter.notifyDataSetChanged()
-                // This is because every time when you add the item the input space or the eidt text space will be cleared
-                editText.text.clear()
-            }
-
-        // Selecting and Deleting the items from the list when the delete button is pressed
-        delete.setOnClickListener {
-            val position: SparseBooleanArray = listView.checkedItemPositions
-            val count = listView.count
-            var item = count - 1
-            while (item >= 0) {
-                if (position.get(item))
-                {
-                    adapter.remove(itemlist.get(item))
-                }
-                item--
-            }
-            position.clear()
-            adapter.notifyDataSetChanged()
-        }
-        }
-        *
-         */
     }
 }
