7631ab5198034747e31b49444427632def568a65
poopi67 <42357644+ejach@users.noreply.github.com>
2021-04-11 16:09:19 -0400

    Added HTTP response codes
    Similarly to the previous push, I added the corresponding response codes that are thrown if the database is empty and when the results are displayed.

--- web/backEnd/services/getAllTasks.php
+++ web/backEnd/services/getAllTasks.php
@@ -9,4 +9,11 @@
     $adapter = new databaseAdapterMySQLI($connectionValues);
     $taskDAO = new TaskDAO($adapter);
     $resultArr = $taskDAO->findAll();
-    echo json_encode($resultArr);
+    // If the database is empty, throw the corresponding response code
+    if (!$resultArr) {
+      http_response_code(404);
+    // Otherwise, echo the results from the database
+    } else {
+      http_response_code(200);
+      echo json_encode($resultArr);
+    }
