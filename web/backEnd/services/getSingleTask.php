1c27038e10a3f252e424a2c5006094ad656741ff
matthewheck17 <57510061+matthewheck17@users.noreply.github.com>
2021-04-18 15:50:31 -0400

    refactor editTask

--- web/backEnd/services/getSingleTask.php
+++ web/backEnd/services/getSingleTask.php
@@ -1,14 +1,14 @@
 <?php
-// Used for JSON pretty print
-header('Content-Type: application/json');
 
-include "../db/DatabaseAdapterMySQLI.class.php";
-include "../db/TaskDAO.class.php";
+require_once("../db/DatabaseAdapterMySQLI.class.php");
+require_once("../db/TaskDAO.class.php");
+
+header('Content-Type: application/json'); // Used for JSON pretty print
 
 $connectionValues = array("localhost", "root", "root", "to_do_list");
-
 $adapter = new DatabaseAdapterMySQLI($connectionValues);
 $taskDAO = new TaskDAO($adapter);
+
 // If the ID exists in the database, send the HTTP request and display the JSON results
 if ($taskDAO->findByID($_GET['id'])) {
   http_response_code(200);
