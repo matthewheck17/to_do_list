7ed7b9b33d348e0dad3e2c40218d9df4e4d467ba
Evan <ejach2000@gmail.com>
2021-04-18 17:57:22 -0400

    Used a basic layout for the web portion. Implemented the color scheme that is being used in the Android application Implemented the getAllTasks function to output the tasks on this layout. TODO: - Change the method of showing/submitting the requests using AJAX/JQUERY Implement the other functions of insert/delete

---
+++ web/frontEnd/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8"/>
+<link rel="stylesheet" href="css/style.css">
+<script type="text/javascript" src="js/showForm.js"></script>
+</head>
+<body>
+
+<div id="myDIV" class="header">
+  <h1>To-Do List</h1>
+<form method="post" action="backEnd/services/createTask.php" id="formElement" style="display: none">
+  <input type="text" id="myInput" placeholder="Title">
+  <input type="text" id="myDesc" placeholder="Description">
+  <span onclick="" class="addBtn">Add</span>
+</form>
+
+</div>
+<button type="button" name="button" onclick="showForm()" class="showBtn">Add Task</button>
+
+<ul id="myUL">
+  <?php
+  $jsondata = file_get_contents('http://127.0.0.1/to_do_list/web/backEnd/services/getAllTasks.php');
+
+  $data = json_decode($jsondata, true);
+  foreach ($data AS $d) {
+    echo '<li>' . "<b>" . $d['title'] . "</b> " . $d['content'] . '</li>';
+  }
+  ?>
+</ul>
+</body>
+
+</body>
+</html>
---
+++ web/frontEnd/js/showForm.js
@@ -0,0 +1,9 @@
+// Function that toggles/hides the form
+function showForm() {
+  x =  document.getElementById('formElement');
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
