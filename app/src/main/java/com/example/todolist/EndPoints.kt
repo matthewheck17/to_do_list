e45032e49c50fbebbeab01afa92e4c4acfbe2319
mauleenn <mauleen.ndlovu@outlook.com>
2021-04-20 15:29:31 -0500

    Fixed incorrect url root.

--- app/src/main/java/com/example/todolist/EndPoints.kt
+++ app/src/main/java/com/example/todolist/EndPoints.kt
@@ -1,7 +1,7 @@
 package com.example.todolist
 
 object EndPoints {
-    private val URL_ROOT = "https://10.0.2.2:8888/to_do_list/web/backend/services/services/"
+    private val URL_ROOT = "https://10.0.2.2:8888/to_do_list/web/backEnd/services/"
     val URL_ADD_TASK = URL_ROOT + "createTask.php"
     val URL_EDIT_TASK = URL_ROOT + "EditTask.php"
     val URL_GET_TASK = URL_ROOT + "getAllTasks.php"
