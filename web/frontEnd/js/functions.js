f35383b77747b323a9ab3cec8d06e115eab4dfdf
Evan <ejach2000@gmail.com>
2021-04-25 16:35:41 -0400

    edit functions.js

--- web/frontEnd/js/functions.js
+++ web/frontEnd/js/functions.js
@@ -1,7 +1,7 @@
-const baseUrl =  "https://matthewheck.me/to_do_list/tasks/"
+const baseUrl =  "http://127.0.0.1/to_do_list/web/backEnd/tasks/"
 // Function that toggles/hides the form
-function showForm() {
-  x =  document.getElementById('formElement');
+function showForm(id) {
+  x =  document.getElementById(id);
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
@@ -23,6 +23,43 @@ function deleteTask(id) {
 
   // Converting JSON data to string
   var data = JSON.stringify({ "task_id": id });
+
+  // Sends the data
+  xhr.send(data);
+}
+
+function editForm(id) {
+  // Specify the URL
+  let xhr = new XMLHttpRequest();
+  let url = baseUrl + "?id=" + id;
+  let title = document.getElementById('editTitle');
+  let content = document.getElementById('editContent');
+
+  // Sets the request type to DELETE
+  xhr.open("PUT", url, true);
+
+  // Set the content type to JSON so the DAO can access it and add it to the database
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  // Converting JSON data to string
+  var data = JSON.stringify({ "title": title.value, "content": content.value, "completed": "0" });
+
+  // Sends the data
+  xhr.send(data);
+}
+function completeTask(title, content) {
+  // Specify the URL
+  let xhr = new XMLHttpRequest();
+  let url = baseUrl + "?id=" + id;
+
+  // Sets the request type to DELETE
+  xhr.open("PUT", url, true);
+
+  // Set the content type to JSON so the DAO can access it and add it to the database
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  // Converting JSON data to string
+  var data = JSON.stringify({ "title": title.value, "content": content.value, "completed": "1" });
 
   // Sends the data
   xhr.send(data);
@@ -82,8 +119,11 @@ window.onload = function() {
       console.error(err);
     } else {
       data.forEach(function(o) {
-        document.getElementById("myTable").innerHTML += '<tr><td>' + o.title + '</td><td>' + o.content + '</td></tr>';
+        // Ternary operator that strikes through completed tasks
+        document.getElementById("myTable").innerHTML += '<tr id="title"><td id="tableRow">' + o.title + '</td><td id="content">' + o.content + '</td><button type="button" name="button" onclick="deleteTask('+o.task_id+')" class="deleteBtn" id="deleteBtn"><i class="fas fa-trash-alt"></i></button><button type="button" name="button" onclick="completeTask('+ o.title +','+ o.content +')" class="completeBtn"><i class="fas fa-check"></i></button><button type="button" name="button" onclick="" class="editBtn"><i class="fas fa-edit"></i></button></tr>';
+        document.getElementById("myTable").innerHTML += '<input type="text" name="editTitle" id="editTitle" value="" placeholder="'+o.title+'"><input type="text" name="editContent" id="editContent" placeholder="'+o.content+'" value=""><button type="button" name="button" onclick="editForm('+o.task_id+')" class="submitBtn">Submit</button>';
       });
+
     }
   });
 }
